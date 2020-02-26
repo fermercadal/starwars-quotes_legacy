c80b576623a69d7647282d786b6789aaa52d573a
Fernando Mercadal <fernando.mercadal@darwoft.com>
2020-02-26 15:38:25 -0300

    Updates

---
+++ precache-manifest.a5c94d097ab618b563bc2b95335a72ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d034a777b351e501e89c1a4fb27f339c",
+    "url": "/starwars-quotes/index.html"
+  },
+  {
+    "revision": "2680c72d0985f73e9019",
+    "url": "/starwars-quotes/static/css/main.b65d17ba.chunk.css"
+  },
+  {
+    "revision": "23d31f149ce9928a98f8",
+    "url": "/starwars-quotes/static/js/2.d291d767.chunk.js"
+  },
+  {
+    "revision": "2680c72d0985f73e9019",
+    "url": "/starwars-quotes/static/js/main.27f6dd8a.chunk.js"
+  },
+  {
+    "revision": "12287922697217c5fd11",
+    "url": "/starwars-quotes/static/js/runtime-main.6d02cf17.js"
+  }
+]);
